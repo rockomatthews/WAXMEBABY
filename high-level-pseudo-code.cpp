b304cd5b1c0cd0c9bed724445f255e6e3677b767
rockomatthews <rob@robmatthe.ws>
2023-11-09 15:08:29 -0600

    add front end with vite

--- high-level-pseudo-code.cpp
+++ high-level-pseudo-code.cpp
@@ -62,10 +62,57 @@ public:
             new_campaign.is_active = true; });
     }
 
+    // Listen for transfer notifications from the eosio.token contract
+    [[eosio::on_notify("eosio.token::transfer")]] void on_transfer(const name &from, const name &to, const asset &amount, const std::string &memo)
+    {
+        // Ensure this is an incoming transfer to the crowdfunding contract
+        if (to != get_self())
+            return;
+
+        // Parse the memo to extract the campaign_id
+        // In a real-world scenario, you'd want to add error checking here
+        uint64_t campaign_id = std::stoull(memo);
+
+        // Find the campaign
+        auto campaign_itr = _campaigns.find(campaign_id);
+        check(campaign_itr != _campaigns.end(), "Campaign does not exist");
+        check(campaign_itr->is_active, "Campaign is not active");
+        check(campaign_itr->deadline.utc_seconds > current_time_point().sec_since_epoch(), "Campaign deadline has passed");
+        check(campaign_itr->goal_amount.symbol == amount.symbol, "Contribution symbol does not match campaign goal symbol");
+
+        // Update the campaign's current amount
+        _campaigns.modify(campaign_itr, get_self(), [&](auto &mod_campaign)
+                          { mod_campaign.current_amount += amount; });
+    }
+
+    // Action to withdraw funds by the creator if the campaign is successful
+    [[eosio::action]] void withdraw(const name &creator, const uint64_t &campaign_id)
+    {
+        require_auth(creator);
+
+        // Find the campaign
+        auto campaign_itr = _campaigns.find(campaign_id);
+        check(campaign_itr != _campaigns.end(), "Campaign does not exist");
+        check(campaign_itr->creator == creator, "Only the campaign creator can withdraw funds");
+        check(campaign_itr->is_active, "Campaign is not active");
+        check(campaign_itr->deadline.utc_seconds <= current_time_point().sec_since_epoch(), "Campaign deadline has not passed yet");
+        check(campaign_itr->current_amount >= campaign_itr->goal_amount, "Campaign has not reached its funding goal");
+
+        // Transfer funds from the contract to the creator
+        action(
+            permission_level{get_self(), "active"_n},
+            "eosio.token"_n, "transfer"_n,
+            std::make_tuple(get_self(), creator, campaign_itr->current_amount, std::string("Campaign funding withdrawal")))
+            .send();
+
+        // Deactivate the campaign
+        _campaigns.modify(campaign_itr, same_payer, [&](auto &mod_campaign)
+                          { mod_campaign.is_active = false; });
+    }
     // ... other actions ...
 
 private:
     // Private helper functions
 };
 
-EOSIO_DISPATCH(crowdfunding, (createcamp))
+EOSIO_DISPATCH(crowdfunding, (createcamp)(contribute)(withdraw))
